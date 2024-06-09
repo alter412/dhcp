8e1eafa6e5bc1ebef3fced72e69c472ada396c0a
alter412 <139802395+alter412@users.noreply.github.com>
2024-06-09 18:31:10 +0530

    wdv

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,41 +1,77 @@
 import styled from "styled-components";
+import {auth, provider} from "../firebase";
+import {signInWithPopup } from "firebase/auth";
+import { useDispatch, useSelector } from "react-redux";
+import {useHistory} from 'react-router-dom';
+import { selectUserName,selectUserEmail,selectUserPhoto , setUserLoginDetails, setSignOutState} from "../features/user/userSlice"; 
+
 
 const Header = (props) =>{
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const userName = useSelector(selectUserName);
+    // const userEmail = useSelector(selectUserEmail);
+    const userPhoto = useSelector(selectUserPhoto);
+
+
+
+    const handleAuth= ()=>{
+        signInWithPopup(auth, provider).
+                    then((result) => {
+                            setUser(result.user); 
+                }).catch((err)=>{
+                                alert(err.message);
+                        })
+    }
+
+    const setUser = (user) =>{
+        dispatch(setUserLoginDetails({
+            name:user.displayName,
+            email:user.email,
+            photo:user.photoURL
+        }));
+    }
 
     return (
         <Nav>
             <Logo>
                 <img src="/images/logo.svg" alt="Disney+"/>
             </Logo>
-            <NavMenu>
-                <a href="/home"  >
-                    <img src="/images/home-icon.svg" alt="HOME"/>
-                    <span>HOME</span>
-                </a>
-                <a>
-                    <img src="/images/search-icon.svg" alt="SEARCH"/>
-                    <span>SEARCH</span>
-                </a>
-                <a>
-                    <img src="/images/watchlist-icon.svg" alt="WATCHLIST"/>
-                    <span>WATCHLIST</span>
-                </a>
-                <a>
-                    <img src="/images/original-icon.svg" alt="ORIGINALS"/>
-                    <span>ORIGINALS</span>
-                </a>
-                <a>
-                    <img src="/images/movie-icon.svg" alt="MOVIES"/>
-                    <span>MOVIES</span>
-                </a>
-                <a>
-                    <img src="/images/series-icon.svg" alt="SERIES"/>
-                    <span>SERIES</span>
-                </a>
-            </NavMenu>
-            <Login>
+            {
+                !userName?
+                <Login onClick={handleAuth}>
                 Login
-            </Login>
+            </Login>:
+            <>
+            <NavMenu>
+            <a href="/home"  >
+                <img src="/images/home-icon.svg" alt="HOME"/>
+                <span>HOME</span>
+            </a>
+            <a>
+                <img src="/images/search-icon.svg" alt="SEARCH"/>
+                <span>SEARCH</span>
+            </a>
+            <a>
+                <img src="/images/watchlist-icon.svg" alt="WATCHLIST"/>
+                <span>WATCHLIST</span>
+            </a>
+            <a>
+                <img src="/images/original-icon.svg" alt="ORIGINALS"/>
+                <span>ORIGINALS</span>
+            </a>
+            <a>
+                <img src="/images/movie-icon.svg" alt="MOVIES"/>
+                <span>MOVIES</span>
+            </a>
+            <a>
+                <img src="/images/series-icon.svg" alt="SERIES"/>
+                <span>SERIES</span>
+            </a>
+        </NavMenu>
+        <UserImg src={userPhoto} alt={userName}/>
+        </>
+            }
         </Nav>
     );
 }
@@ -159,4 +195,10 @@ color:#000;
 border-color:transparent;
 }
 `;
+
+const UserImg = styled.img`
+    height:100%;
+    
+`;
+
 export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBkhnInkusjIhe1Hhm6_U8RhyQqahNUIUU",
+  authDomain: "dhcp-6f6e7.firebaseapp.com",
+  projectId: "dhcp-6f6e7",
+  storageBucket: "dhcp-6f6e7.appspot.com",
+  messagingSenderId: "831707234931",
+  appId: "1:831707234931:web:da3c7050dc872296666006",
+  measurementId: "G-TSE8EXQLG8"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
+//const db = firebaseApp.firestore();
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+
+export {auth , provider};
+export default {};
